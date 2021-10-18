2f07c9595057d7773520b0308775a44c3f074b58
juanielias12345 <juanielias@icloud.com>
2021-10-18 13:35:51 -0300

    mercadoliebre2.0

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor corriendo en el puerto 3000")
+})
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/formDeRegistro.html'))
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/formDeLogin.html'))
+})
